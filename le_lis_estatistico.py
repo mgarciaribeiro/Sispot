9d7c608ace67b425d5a1d06543b2b4da846a348f
Matheus Ribeiro <72823862+mgarciaribeiro@users.noreply.github.com>
2021-08-25 17:29:05 -0300

    Update le_lis_estatistico.py

--- le_lis_estatistico.py
+++ le_lis_estatistico.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats
 
 def histograma(vet_1, vet_2):
     """"
@@ -49,8 +50,23 @@ with open(leitura, 'r') as arquivo:
 
             # Pega tipo de variável e nome da variável
 
-            atp_estat['Tipo'].append(texto[linhas - 3][51:53])
-            atp_estat['Variavel'].append(texto[linhas - 3][65:100])
+            if '0' in texto[linhas - 3][51:53]:
+
+                atp_estat['Tipo'].append('Tensao FT')
+                atp_estat['Variavel'].append(texto[linhas - 3][65:70])
+
+            elif '-1' in texto[linhas - 3][51:53]:
+
+                atp_estat['Tipo'].append('Tensão FF')
+                atp_estat['Variavel'].append(texto[linhas - 3][65:70])
+
+            else:
+
+                atp_estat['Tipo'].append('C. Inrush')
+                atp_estat['Variavel'].append(texto[linhas - 3][65:70] + ' - ' + texto[linhas - 3][71:76])
+
+#            atp_estat['Tipo'].append(texto[linhas - 3][51:53])
+#            atp_estat['Variavel'].append(texto[linhas - 3][65:100])
 
             j = 0
             val_aux = []
@@ -75,7 +91,29 @@ atp_estat = pd.DataFrame(atp_estat)
 
 #sns.distplot(atp_estat[0]['Valores'])
 
-sns.displot(atp_estat.iloc[0]['Valores'])
-plt.show()
+# Plotagem dos gráficos
 
-#print(atp_estat.iloc[0]['Valores'])
+for i in range(len(atp_estat)):
+
+    if i == 0 or i % 8 == 0:
+
+        plt.subplots(2, 4)
+
+#        plt.figure()
+        plt.subplot(2, 4, 1)
+
+    else:
+
+        plt.subplot(2, 4, (i % 8) + 1)
+
+    sns.histplot(atp_estat.iloc[i]['Valores'], kde=True, stat = 'density')
+#    plt.plot(atp_estat.iloc[i]['Valores'], stats.norm(atp_estat.iloc[i]['Valores'].mean(), atp_estat.iloc[i]['Valores'].std()).pdf(atp_estat.iloc[i]['Valores']),
+#             label='Normal Padrão')
+
+    plt.plot(atp_estat.iloc[i]['Valores'], stats.norm(np.mean(atp_estat.iloc[i]['Valores']), np.std(atp_estat.iloc[i]['Valores'])).pdf(atp_estat.iloc[i]['Valores']),
+             label='Normal Padrão', color = 'red')
+
+    plt.legend(loc=0)
+    plt.title(f'{atp_estat.iloc[i]["Tipo"]} {atp_estat.iloc[i]["Variavel"]}')
+
+plt.show()
